28f2056072f249fee4ea63acc46475951a818121
karol-kot2007 <karol.kot.2007@gmail.com>
2023-01-07 15:24:24 +0100

    move sprite

--- SDL++Client/SDL++Client.cpp
+++ SDL++Client/SDL++Client.cpp
@@ -12,50 +12,71 @@
 #include <Input.h>
 
 #undef main
+using namespace SDL;
+/// <summary>
+/// 
+/// </summary>
+void moveSprite(AnimatedSprite* sprite, int deltaX, int deltaY)
+{
+	auto pos = sprite->position();
+	pos.x += deltaX;
+	pos.y += deltaY;
+	sprite->setPosition(pos);
 
 
+}
+
+/// <summary>
+/// 
+/// </summary>
+/// <returns></returns>
 int main()
 {
 
-  auto window = SDL::createWindow();
-  auto sprite = SDL::createAnimatedSprite(window, "Images\\hero.bmp", { 0, 0 });
-  
-  bool quit = false;
-  SDL::Input input;
-  bool redraw = true;
-  while (!quit)
-  {
-    input.update();
-    if (input.isKeyDown(SDLK_LEFT))
-    {
-      auto pos = sprite->position();
-      pos.x -= 5;
-      sprite->setPosition(pos);
-      
-      redraw = true;
-    }
-    else if (input.isKeyDown(SDLK_RIGHT))
-    {
-      auto pos = sprite->position();
-      pos.x += 5;
-      sprite->setPosition(pos);
+	auto window = SDL::createWindow();
+	auto sprite = SDL::createAnimatedSprite(window, "Images\\hero.bmp", { 0, 0 });
 
-      redraw = true;
-    }
+	
 
-    else if (input.isKeyDown(SDLK_ESCAPE))
-      quit = true;
+	bool quit = false;
+	SDL::Input input;
+	bool redraw = true;
+	int speed = 128;
+	while (!quit)
+	{
+		input.update();
+		if (input.isKeyDown(SDLK_LEFT))
+		{
+			moveSprite(sprite.get(), -1*speed, 0);
+			redraw = true;
+		}
+		else if (input.isKeyDown(SDLK_RIGHT))
+		{
+			moveSprite(sprite.get(), 1 * speed, 0);
+			redraw = true;
+		}
+		else if (input.isKeyDown(SDLK_UP))
+		{
+			moveSprite(sprite.get(), 0, -1 * speed);
+			redraw = true;
+		}
+		else if (input.isKeyDown(SDLK_DOWN))
+		{
+			moveSprite(sprite.get(), 0, 1 * speed);
+			redraw = true;
+		}
+		else if (input.isKeyDown(SDLK_ESCAPE))
+			quit = true;
 
-    if (redraw)
-    {
-      window->render(sprite);
+		if (redraw)
+		{
+			window->render(sprite);
+		}
+		redraw = false;
+		SDL::delay(10);
+	}
 
-    }
-    redraw = false;
-    SDL::delay(10);
-  }
-
-  return 0;
+	return 0;
 }
 
 
