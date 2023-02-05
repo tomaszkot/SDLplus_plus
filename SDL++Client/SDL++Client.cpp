08ec3f2d6a7163df04052947fda60426c6e2adc6
karol-kot2007 <karol.kot.2007@gmail.com>
2023-02-05 13:49:09 +0100

    classs aded

--- SDL++Client/SDL++Client.cpp
+++ SDL++Client/SDL++Client.cpp
@@ -3,6 +3,7 @@
 
 #include "stdafx.h"
 
+#include <vector>
 #include <stdio.h>
 #include <Window.h>
 #include <Surface.h>
@@ -16,63 +17,117 @@ using namespace SDL;
 int width = 1920;// SDL::SCREEN_WIDTH;
 int height = 1080;
 const int spriteSize = 128;
-const int numTrees = 10;
-const int numEnemies = 4;
+//const int numTrees = 10;
+//const int numEnemies = 4;
 //std::unique_ptr<Window> window;
-std::unique_ptr<AnimatedSprite> trees[numTrees];
-std::unique_ptr<AnimatedSprite> enemies[numEnemies];
-/// <summary>
-/// 
-/// </summary>
-void moveSprite(AnimatedSprite* sprite, int deltaX, int deltaY)
-{
-	auto pos = sprite->position();
-	auto newX = pos.x + deltaX;
-	pos.x = newX;
 
-	if (pos.x >= 1920)
+
+class Game
+{
+private:
+	std::vector<std::unique_ptr<AnimatedSprite>> m_trees;
+	std::vector<std::unique_ptr<AnimatedSprite>> enemies;
+
+public:
+
+	void create(std::unique_ptr<Window>& window)
 	{
-		return;
-	}
-	else if (pos.x < 0)
-	{
-		return;
-	}
-	pos.y += deltaY;
-	if (pos.y > 1080)
-	{
-		return;
-	}
-	else if (pos.y < 0)
-	{
-		return;
-	}
-	for (int i = 0; i < numTrees; i++)
-	{
-		auto treePos=trees[i]->position();
-		if (treePos.x == pos.x && treePos.y==pos.y)
+		for (int i = 0; i < 10; i++)
 		{
-			return;
+			m_trees.push_back(createSprite(window, "Images\\tree.bmp"));
+		}
+		for (int i = 0; i < 4; i++)
+		{
+			enemies.push_back(createSprite(window, "Images\\bear.bmp"));
+		}
+
+	}
+
+	void render(std::unique_ptr<Window>& window)
+	{
+		for (auto& i : m_trees)
+		{
+			window->render(i);
+		}
+
+		for (int i = 0; i < enemies.size(); i++)
+		{
+			window->render(enemies[i]);
 		}
 	}
 
-	sprite->setPosition(pos);
+	//std::unique_ptr<AnimatedSprite> drawSprites(numTrees);
+	/// <summary>
+	/// 
+	/// </summary>
+	void moveSprite(AnimatedSprite* sprite, int deltaX, int deltaY)
+	{
+		auto pos = sprite->position();
+		auto newX = pos.x + deltaX;
+		pos.x = newX;
+
+		if (pos.x >= 1920)
+		{
+			return;
+		}
+		else if (pos.x < 0)
+		{
+			return;
+		}
+		pos.y += deltaY;
+		if (pos.y > 1080)
+		{
+			return;
+		}
+		else if (pos.y < 0)
+		{
+			return;
+		}
+		for (int i = 0; i < m_trees.size(); i++)
+		{
+			auto treePos = m_trees[i]->position();
+			if (treePos.x == pos.x && treePos.y == pos.y)
+			{
+				return;
+			}
+
+		}
+		for (int i = 0; i < enemies.size(); i++)
+		{
+			auto enemyPos = enemies[i]->position();
+			if (enemyPos.x == pos.x && enemyPos.y == pos.y)
+			{
+				return;
+			}
 
 
-}
-std::unique_ptr<AnimatedSprite> createSprite(std::unique_ptr<Window>* window,std::string spriteImage )
-{
-	int a = 3;
-	int ay = 1;
-	int by = 10;
-	int bx = 15;
-	int dx = rand() % (bx - a) + a;
-	int dy = rand() % (by - a) + ay;
-	auto sprite = SDL::createAnimatedSprite(*window, spriteImage, { spriteSize * dx,spriteSize * dy });
-	return sprite;
-}
+		}
+		//1) jak jest enemy to dac return
+
+		sprite->setPosition(pos);
 
 
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="window"></param>
+	/// <param name="spriteImage"></param>
+	/// <returns></returns>
+	std::unique_ptr<AnimatedSprite> createSprite(std::unique_ptr<Window>& window, std::string spriteImage)
+	{
+		int a = 3;
+		int ay = 1;
+		int by = 10;
+		int bx = 15;
+		int dx = rand() % (bx - a) + a;
+		int dy = rand() % (by - a) + ay;
+		auto sprite = SDL::createAnimatedSprite(window, spriteImage, { spriteSize * dx,spriteSize * dy });
+		return sprite;
+	}
+
+};
 
 
 
@@ -86,17 +141,13 @@ int main()
 
 	auto window = SDL::createWindow();
 	auto hero = SDL::createAnimatedSprite(window, "Images\\hero.bmp", { 0, 0 });
+	Game game;
+	game.create(window);
+	//3
+	//createSprites(trees, numTrees, &window,"Images\\tree.bmp");
+	//createSprites(enemies, numEnemies, &window,"Images\\bear.bmp");
 	
-
 	
-	for (int i = 0; i < numTrees; i++)
-	{
-		trees[i] = createSprite(&window,"Images\\tree.bmp");
-	}
-	for (int i = 0; i < numEnemies; i++)
-	{
-		enemies[i] =createSprite(&window,"Images\\bear.bmp");
-	}
 	bool quit = false;
 	SDL::Input input;
 	bool redraw = true;
@@ -106,22 +157,22 @@ int main()
 		input.update();
 		if (input.isKeyDown(SDLK_LEFT))
 		{
-			moveSprite(hero.get(), -1 * speed, 0);
+			game.moveSprite(hero.get(), -1 * speed, 0);
 			redraw = true;
 		}
 		else if (input.isKeyDown(SDLK_RIGHT))
 		{
-			moveSprite(hero.get(), 1 * speed, 0);
+			game.moveSprite(hero.get(), 1 * speed, 0);
 			redraw = true;
 		}
 		else if (input.isKeyDown(SDLK_UP))
 		{
-			moveSprite(hero.get(), 0, -1 * speed);
+			game.moveSprite(hero.get(), 0, -1 * speed);
 			redraw = true;
 		}
 		else if (input.isKeyDown(SDLK_DOWN))
 		{
-			moveSprite(hero.get(), 0, 1 * speed);
+			game.moveSprite(hero.get(), 0, 1 * speed);
 			redraw = true;
 		}
 		else if (input.isKeyDown(SDLK_ESCAPE))
@@ -132,15 +183,11 @@ int main()
 			window->clear();
 			window->render(hero);
 			
-			for (int i = 0; i < numTrees; i++)
-			{
-				window->render(trees[i]);
-			}
-
-			for (int i = 0; i < numEnemies; i++)
-			{
-				window->render(enemies[i]);
-			}
+			//2
+			//drawSprites(trees, numTrees);
+			//drawSprites(enemies, numEnemies);
+			
+			game.render(window);
 		}    
 		redraw = false;
 		SDL::delay(10);
